e83b0871b75178381c48220f1906b412f00c3e21
nkashyap14 <nikhilesh.kashyap2903@gmail.com>
2024-11-05 10:42:20 -0500

    implemented basic config

---
+++ src/config.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import Optional
+
+class DriveConfig:
+
+    '''
+    Simple configuration class implemented utilizing the singleton pattern for the strac takehome test. Only one configuration throughout application life cycle. 
+    Functions based on the assumption that the user has a .gdrive directory and if one doesn't exist is created for the user. From there two json files named credentials.json and secrets.json
+    '''
+
+    _instance: Optional['DriveConfig'] = None
+
+    def __new__(cls) -> 'DriveConfig':
+        if cls._instance is None:
+            cls._instance = super(DriveConfig, cls).__new__(cls)
+            cls._instance.initialized = False
+        return cls._instance
+    
+    def __init__(self) -> None:
+        if getattr(self, '_initialized', False):
+            return
+        
+        self.config_dir = Path.home() / '.gdrive'
+        self.credentials = self.config_dir / 'credentials.json'
+        self.secrets = self.config_dir / 'secrets.json'
+
+        self.scopes = [
+            'https://www.googleapis.com/auth/drive.file',  # For file operations
+            'https://www.googleapis.com/auth/drive.metadata.readonly'  # For listing files
+        ]
+
+        # Create config directory if it doesn't exist
+        self.config_dir.mkdir(parents=True, exist_ok=True)
+
+        self._initialized = True
